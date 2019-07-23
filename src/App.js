aa7fb81e62b8287b7e6af48417e60aef2adad80b
vandna kapoor <vandnakapoor19@gmail.com>
2019-07-23 18:22:23 -0400

    Implemented Search Functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
-import {CardList} from './components/card-list/';
+import { CardList } from './components/card-list/';
 import './App.css';
-class App extends Component{
- 
-  constructor(){
+class App extends Component {
+  constructor() {
     super();
-      this.state = {
-        monsters:[]
-      }
+    this.state = {
+      monsters: [],
+      seaechField: ''
+    };
   }
-  componentDidMount(){
-    fetch('http://jsonplaceholder.typicode.com/users').
-    then(response=>response.json()).
-    then(users=>this.setState({monsters:users}));
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
   render() {
+    const { monsters, seaechField } = this.state;
+    const monsterData = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(seaechField.toLowerCase())
+    );
     return (
-      <div className="App">
-        <CardList monsters={this.state.monsters}/>
+      <div className='App'>
+        <input
+          type='search'
+          name='searchField'
+          placeholder='SearchByKeyword'
+          onChange={e => this.setState({ seaechField: e.target.value })}
+        />
+        <CardList monsters={monsterData} />
       </div>
     );
   }
